67f38fc6c39a4422d3d790b63bc3514ee1c0b8d4
jaredsbror <electricalengineeringexpanded@gmail.com>
2024-05-02 12:19:40 -0600

    (phase0) added all chess piece row and column constants

--- shared/src/main/java/chess/Constants.java
+++ shared/src/main/java/chess/Constants.java
@@ -1,9 +1,49 @@
 package chess;
 
+
+/*
+KING,
+QUEEN,
+BISHOP,
+KNIGHT,
+ROOK,
+PAWN
+ */
+
 public interface Constants {
     // Board dimensions
     int BOARD_NUM_ROWS = 8;
     int BOARD_NUM_COLUMNS = 8;
 
-    // Default piece locations
+    // Default black piece locations
+    int BLACK_KING_ROW = 7;
+    int BLACK_KING_COL = 4;
+    int BLACK_QUEEN_ROW = 7;
+    int BLACK_QUEEN_COL = 3;
+    int BLACK_BISHOP_ROW = 7;
+    int BLACK_BISHOP_COL_1 = 2;
+    int BLACK_BISHOP_COL_2 = 5;
+    int BLACK_KNIGHT_ROW = 7;
+    int BLACK_KNIGHT_COL_1 = 1;
+    int BLACK_KNIGHT_COL_2 = 6;
+    int BLACK_ROOK_ROW = 7;
+    int BLACK_ROOK_COL_1 = 0;
+    int BLACK_ROOK_COL_2 = 7;
+    int BLACK_PAWN_ROW = 6;
+
+    // Default white piece locations
+    int WHITE_KING_ROW = 0;
+    int WHITE_KING_COL = 4;
+    int WHITE_QUEEN_ROW = 0;
+    int WHITE_QUEEN_COL = 3;
+    int WHITE_BISHOP_ROW = 0;
+    int WHITE_BISHOP_COL_1 = 2;
+    int WHITE_BISHOP_COL_2 = 5;
+    int WHITE_KNIGHT_ROW = 0;
+    int WHITE_KNIGHT_COL_1 = 1;
+    int WHITE_KNIGHT_COL_2 = 6;
+    int WHITE_ROOK_ROW = 0;
+    int WHITE_ROOK_COL_1 = 0;
+    int WHITE_ROOK_COL_2 = 7;
+    int WHITE_PAWN_ROW = 1;
 }
