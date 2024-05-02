a1b7241d18ad6f58808047ceaffb7227f52b19fb
jaredsbror <electricalengineeringexpanded@gmail.com>
2024-05-02 13:02:36 -0600

    (phase0) Implemented getStartPosition, getEndPosition, and getPromotionPiece in ChessMove.java

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -25,14 +25,14 @@ public class ChessMove {
      * @return ChessPosition of starting location
      */
     public ChessPosition getStartPosition() {
-        throw new RuntimeException("Not implemented");
+        return startPosition;
     }
 
     /**
      * @return ChessPosition of ending location
      */
     public ChessPosition getEndPosition() {
-        throw new RuntimeException("Not implemented");
+        return  endPosition;
     }
 
     /**
@@ -41,7 +41,9 @@ public class ChessMove {
      *
      * @return Type of piece to promote a pawn to, or null if no promotion
      */
-    public ChessPiece.PieceType getPromotionPiece() {throw new RuntimeException("Not implemented"); }
+    public ChessPiece.PieceType getPromotionPiece() {
+        return promotionPiece;
+    }
 
     //
     @Override
