655692ccdcb3cd4a439a3c6d5da19d1ffc00fd22
jaredsbror <electricalengineeringexpanded@gmail.com>
2024-05-02 11:14:17 -0600

    (phase0) added and/or finished getTeamColor, setTeamColor, getPieceType, and setPieceType methods

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -10,7 +10,12 @@ import java.util.Collection;
  */
 public class ChessPiece {
 
+    private ChessGame.TeamColor pieceColor;
+    private ChessPiece.PieceType type;
+
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+        this.pieceColor = pieceColor;
+        this.type = type;
     }
 
     /**
@@ -29,14 +34,24 @@ public class ChessPiece {
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+        return pieceColor;
+    }
+
+    // Set team color
+    public void setTeamColor(ChessGame.TeamColor pieceColor) {
+        this.pieceColor = pieceColor;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+        return type;
+    }
+
+    // Set piece type
+    public void setPieceType(ChessPiece.PieceType type) {
+        this.type = type;
     }
 
     /**
