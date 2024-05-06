8a54db8ddd27b4ec16435b55cb0963d60d175684
jaredsbror <electricalengineeringexpanded@gmail.com>
2024-05-06 10:36:00 -0600

    (phase0)  Updating MoveCalculator classes to include current team color, getCurrentTeamColor, setCurrentTeamColor, and have currentTeamColor in constructor

--- shared/src/main/java/chess/KingMovesCalculator.java
+++ shared/src/main/java/chess/KingMovesCalculator.java
@@ -7,9 +7,12 @@ import java.util.List;
 public class KingMovesCalculator implements PieceMovesCalculator {
 
     // Constructor
-    public KingMovesCalculator() {
+    public KingMovesCalculator(ChessGame.TeamColor currentTeamColor) {
         // Collection to hold resulting valid moves
         validMoves = new ArrayList<>();
+        // Current and opponent team color
+        this.currentTeamColor = currentTeamColor;
+        opponentTeamColor = (currentTeamColor == ChessGame.TeamColor.WHITE ? ChessGame.TeamColor.BLACK : ChessGame.TeamColor.WHITE);
         // Various row and column variables for the current player
         currentRow = 0;
         currentColumn = 0;
@@ -27,6 +30,9 @@ public class KingMovesCalculator implements PieceMovesCalculator {
 
     // Collection to hold resulting valid moves
     private List<ChessMove> validMoves;
+    // Current and opponent team color
+    private ChessGame.TeamColor currentTeamColor;
+    private ChessGame.TeamColor opponentTeamColor;
     // Various row and column variables for the current player
     private int currentRow;
     private int currentColumn;
@@ -39,6 +45,22 @@ public class KingMovesCalculator implements PieceMovesCalculator {
     private int opponentKingRowMax;
     private int opponentKingColMin;
     private int opponentKingColMax;
+
+    // Get current team color
+    public ChessGame.TeamColor getCurrentTeamColor() {
+        return currentTeamColor;
+    }
+
+    // Get opponent team color
+    public ChessGame.TeamColor getOpponentTeamColor() {
+        return opponentTeamColor;
+    }
+
+    // Set current (and opponent) team color
+    public void setCurrentTeamColor(ChessGame.TeamColor currentTeamColor) {
+        this.currentTeamColor = currentTeamColor;
+        opponentTeamColor = (currentTeamColor == ChessGame.TeamColor.WHITE ? ChessGame.TeamColor.BLACK : ChessGame.TeamColor.WHITE);
+    }
 
     // Frequently used check in the method pieceMoves
     private boolean isMoveValid() {
--- shared/src/main/java/chess/RookMovesCalculator.java
+++ shared/src/main/java/chess/RookMovesCalculator.java
@@ -7,9 +7,12 @@ import java.util.List;
 public class RookMovesCalculator implements PieceMovesCalculator {
 
     // Constructor
-    public RookMovesCalculator() {
+    public RookMovesCalculator(ChessGame.TeamColor currentTeamColor) {
         // Collection to hold resulting valid moves
         validMoves = new ArrayList<>();
+        // Current and opponent team color
+        this.currentTeamColor = currentTeamColor;
+        opponentTeamColor = (currentTeamColor == ChessGame.TeamColor.WHITE ? ChessGame.TeamColor.BLACK : ChessGame.TeamColor.WHITE);
         // Various row and column variables for the current player
         currentRow = 0;
         currentColumn = 0;
@@ -19,11 +22,30 @@ public class RookMovesCalculator implements PieceMovesCalculator {
 
     // Collection to hold resulting valid moves
     private List<ChessMove> validMoves;
+    // Current and opponent team color
+    private ChessGame.TeamColor currentTeamColor;
+    private ChessGame.TeamColor opponentTeamColor;
     // Various row and column variables for the current player
     private int currentRow;
     private int currentColumn;
     private int possibleRow;
     private int possibleColumn;
+
+    // Get current team color
+    public ChessGame.TeamColor getCurrentTeamColor() {
+        return currentTeamColor;
+    }
+
+    // Get opponent team color
+    public ChessGame.TeamColor getOpponentTeamColor() {
+        return opponentTeamColor;
+    }
+
+    // Set current (and opponent) team color
+    public void setCurrentTeamColor(ChessGame.TeamColor currentTeamColor) {
+        this.currentTeamColor = currentTeamColor;
+        opponentTeamColor = (currentTeamColor == ChessGame.TeamColor.WHITE ? ChessGame.TeamColor.BLACK : ChessGame.TeamColor.WHITE);
+    }
 
     // Check if a position is on the board
     private boolean isWithinBounds() {
