183e248e52cc2828e76e54eed360cfd11df0677f
jaredsbror <electricalengineeringexpanded@gmail.com>
2024-05-23 09:24:33 -0600

    (phase3) Fixed some organization problems and added handler instance variables to server

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,9 +1,31 @@
 package server;
 
+import handlers.*;
 import spark.*;
 
 public class Server {
 
+    // Handler instances to call on
+    private ClearApplicationHandler clearApplicationHandler;
+    private CreateGameHandler createGameHandler;
+    private JoinGameHandler joinGameHandler;
+    private ListGamesHandler listGamesHandler;
+    private LoginHandler loginHandler;
+    private LogoutHandler logoutHandler;
+    private RegisterHandler registerHandler;
+
+    // Constructor
+    public Server(ClearApplicationHandler clearApplicationHandler, CreateGameHandler createGameHandler, handlers.JoinGameHandler joinGameHandler, ListGamesHandler listGamesHandler, LoginHandler loginHandler, LogoutHandler logoutHandler, RegisterHandler registerHandler) {
+        this.clearApplicationHandler = clearApplicationHandler;
+        this.createGameHandler = createGameHandler;
+        this.joinGameHandler = joinGameHandler;
+        this.listGamesHandler = listGamesHandler;
+        this.loginHandler = loginHandler;
+        this.logoutHandler = logoutHandler;
+        this.registerHandler = registerHandler;
+    }
+
+    // Run the server
     public int run(int desiredPort) {
         Spark.port(desiredPort);
 
@@ -15,10 +37,12 @@ public class Server {
         return Spark.port();
     }
 
+    // Get the server port
     public int port() {
         return Spark.port();
     }
 
+    // Stop the server
     public void stop() {
         Spark.stop();
         Spark.awaitStop();
