99bffdd74e3ba786d460362e25f3a8919f9171d4
jaredsbror <electricalengineeringexpanded@gmail.com>
2024-05-23 11:29:42 -0600

    (phase3) Added in example server code

---
+++ server/src/main/java/server/SimpleHelloBYUServer.java
@@ -0,0 +1,9 @@
+package server;
+
+import spark.Spark;
+
+public class SimpleHelloBYUServer {
+    public static void main(String[] args) {
+        Spark.get("/hello", (req, res) -> "Hello BYU!");
+    }
+}
