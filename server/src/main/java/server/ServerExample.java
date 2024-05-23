09a54e8c96e5b3fb41417eb5d3ba03413615a5c8
jaredsbror <electricalengineeringexpanded@gmail.com>
2024-05-23 11:09:22 -0600

    (phase3) Fixed problem with response/request names

---
+++ server/src/main/java/server/ServerExample.java
@@ -0,0 +1,41 @@
+package server;
+
+import com.google.gson.Gson;
+import spark.*;
+import java.util.*;
+
+public class ServerExample {
+    private ArrayList<String> names = new ArrayList<>();
+
+    public static void main(String[] args) {
+        new ServerExample().run();
+    }
+
+    private void run() {
+        // Specify the port you want the server to listen on
+        Spark.port(8080);
+
+        // Register a directory for hosting static files
+        Spark.externalStaticFileLocation("public");
+
+        // Register handlers for each endpoint using the method reference syntax
+        Spark.post("/name/:name", this::addName);
+        Spark.get("/name", this::listNames);
+        Spark.delete("/name/:name", this::deleteName);
+    }
+
+    private Object addName(Request req, Response res) {
+        names.add(req.params(":name"));
+        return listNames(req, res);
+    }
+
+    private Object listNames(Request req, Response res) {
+        res.type("application/json");
+        return new Gson().toJson(Map.of("name", names));
+    }
+
+    private Object deleteName(Request req, Response res) {
+        names.remove(req.params(":name"));
+        return listNames(req, res);
+    }
+}
