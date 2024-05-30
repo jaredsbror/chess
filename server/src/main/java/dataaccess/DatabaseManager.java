7a1b238d837d1d7672bd6aa0a8cd0496f0583c24
jaredsbror <electricalengineeringexpanded@gmail.com>
2024-05-30 14:16:29 -0600

    (phase 4) Making progress, lots of small pushes.

--- server/src/main/java/dataaccess/DatabaseManager.java
+++ server/src/main/java/dataaccess/DatabaseManager.java
@@ -9,6 +9,12 @@ public class DatabaseManager {
     private static final String PASSWORD;
     private static final String CONNECTION_URL;
 
+    enum TableChoice {
+        AUTHTABLE,
+        GAMETABLE,
+        USERTABLE
+    }
+
     /*
      * Load the database information for the db.properties file.
      */
@@ -24,7 +30,7 @@ public class DatabaseManager {
 
                 var host = props.getProperty("db.host");
                 var port = Integer.parseInt(props.getProperty("db.port"));
-                CONNECTION_URL = String.format("jdbc:mysql://%s:%d", host, port);
+                CONNECTION_URL = String.format("jdbc:mysql://%s:%d/%s", host, port, DATABASE_NAME);
             }
         } catch (Exception ex) {
             throw new RuntimeException("unable to process db.properties. " + ex.getMessage());
@@ -32,13 +38,56 @@ public class DatabaseManager {
     }
 
     /**
-     * Creates the database if it does not already exist.
+     * Initial parameters to set up the authTable in mySQL
      */
-    public static void createDatabase() throws DataAccessException {
+    private static final String[] createAuthTableStatement = {
+            """
+            CREATE TABLE IF NOT EXISTS authTable (
+              username VARCHAR(255) NOT NULL,
+              authToken VARCHAR(255) NOT NULL,
+              PRIMARY KEY (`username`)
+            )
+            """
+    };
+
+    /**
+     * Initial parameters to set up the gameTable in mySQL
+     */
+    private static final String[] createGameTableStatement = {
+            """
+            CREATE TABLE IF NOT EXISTS  gameTable (
+              gameID INT NOT NULL,
+              whiteUsername VARCHAR(255),
+              blackUsername VARCHAR(255),
+              gameName VARCHAR(255) NOT NULL,
+              game TEXT NOT NULL,
+              PRIMARY KEY (`gameID`)
+            )
+            """
+    };
+
+    /**
+     * Initial parameters to set up the userTable in mySQL
+     */
+    private static final String[] createUserTableStatement = {
+            """
+            CREATE TABLE IF NOT EXISTS  userTable (
+              username VARCHAR(255) NOT NULL,
+              password VARCHAR(255) NOT NULL,
+              email VARCHAR(255) NOT NULL,
+              PRIMARY KEY (`username`)
+            )
+            """
+    };
+
+    /**
+     * Creates the database if it does not already exist.
+     * @param statement
+     */
+    static void executeStatementInMySQL(String statement) throws DataAccessException {
         try {
-            var statement = "CREATE DATABASE IF NOT EXISTS " + DATABASE_NAME;
-            var conn = DriverManager.getConnection(CONNECTION_URL, USER, PASSWORD);
-            try (var preparedStatement = conn.prepareStatement(statement)) {
+            Connection connection = DriverManager.getConnection(CONNECTION_URL, USER, PASSWORD);
+            try (PreparedStatement preparedStatement = connection.prepareStatement(statement)) {
                 preparedStatement.executeUpdate();
             }
         } catch (SQLException e) {
@@ -46,39 +95,40 @@ public class DatabaseManager {
         }
     }
 
-    private final String[] createAuthTableStatement = {
-            """
-            CREATE TABLE IF NOT EXISTS  authTable (
-              `username` VARCHAR(255) NOT NULL
-              `authToken` VARCHAR(255) NOT NULL
-              PRIMARY KEY (`username`)
-            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
-            """
-    };
+    /**
+     * Creates the database if it does not already exist.
+     */
+    public static void createDatabase() throws DataAccessException {
+        executeStatementInMySQL("CREATE DATABASE IF NOT EXISTS " + DATABASE_NAME);
+        createTable(TableChoice.AUTHTABLE);
+        createTable(TableChoice.GAMETABLE);
+        createTable(TableChoice.USERTABLE);
+    }
 
-    private final String[] createGameTableStatement = {
-            """
-            CREATE TABLE IF NOT EXISTS  gameTable (
-              `gameID` INT NOT NULL
-              `whiteUsername` VARCHAR(255)
-              `blackUsername` VARCHAR(255)
-              `gameName` VARCHAR(255) NOT NULL
-              `game` LONGTEXT NOT NULL
-              PRIMARY KEY (`gameID`)
-            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
-            """
-    };
-
-    private final String[] createUserTableStatement = {
-            """
-            CREATE TABLE IF NOT EXISTS  userTable (
-              `username` VARCHAR(255) NOT NULL
-              `password` VARCHAR(255) NOT NULL
-              `email` VARCHAR(255) NOT NULL
-              PRIMARY KEY (`username`)
-            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
-            """
-    };
+    /**
+     * Creates a table if it does not already exist.
+     * @param tableChoice (which of the tables to create)
+     */
+    static void createTable(TableChoice tableChoice) throws DataAccessException {
+        String statement;
+        // Depending on tableChoice, get the statement
+        switch (tableChoice) {
+            case AUTHTABLE:
+                statement = String.join(" ", createAuthTableStatement);
+                executeStatementInMySQL(statement);
+                break;
+            case GAMETABLE:
+                statement = String.join(" ", createGameTableStatement);
+                executeStatementInMySQL(statement);
+                break;
+            case USERTABLE:
+                statement = String.join(" ", createUserTableStatement);
+                executeStatementInMySQL(statement);
+                break;
+            default:
+                break;
+        }
+    }
 
     /**
      * Create a connection to the database and sets the catalog based upon the
@@ -100,5 +150,21 @@ public class DatabaseManager {
         } catch (SQLException e) {
             throw new DataAccessException(e.getMessage());
         }
+    }
+
+    public static String getDatabaseName() {
+        return DATABASE_NAME;
+    }
+
+    public static String getUSER() {
+        return USER;
+    }
+
+    public static String getPASSWORD() {
+        return PASSWORD;
+    }
+
+    public static String getConnectionUrl() {
+        return CONNECTION_URL;
     }
 }
--- server/src/main/java/dataaccess/auth/SQLAuthDao.java
+++ server/src/main/java/dataaccess/auth/SQLAuthDao.java
@@ -1,6 +1,46 @@
 package dataaccess.auth;
 
 import dataaccess.AuthDAO;
+import dataaccess.DataAccessException;
+import dataaccess.DatabaseManager;
+import model.original.AuthData;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 public class SQLAuthDao implements AuthDAO {
+
+    private Connection connection;
+    private String statement;
+
+    public SQLAuthDao() throws DataAccessException {
+        try {
+            var conn = DriverManager.getConnection(DatabaseManager.getConnectionUrl(), DatabaseManager.getUSER(), DatabaseManager.getPASSWORD());
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getMessage());
+        }
+    }
+
+    public AuthData getAuthDataGivenAuthToken(String authToken) {
+    }
+
+    public String createAuthToken(String username) {
+    }
+
+    // Verify that an authToken exists in the authTable
+    public Boolean verifyAuthToken(String authToken) {
+    }
+
+    public AuthData deleteAuthDataGivenAuthToken(String authToken) {
+    }
+
+    public void clear() {
+    }
+
+    public Boolean isEmpty() {
+    }
+
+    public String toString() {
+
+    }
 }
