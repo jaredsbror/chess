42fe80718310e3e846a579a7754eda3bbec8f761
jaredsbror <electricalengineeringexpanded@gmail.com>
2024-06-10 16:35:39 -0600

    (phase 5) STarting to work on the menus.

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -16,35 +16,38 @@ public class Main {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
 
-        ServerFacade serverFacade = new ServerFacade();
-        try {
-            ClearResult clearResult = serverFacade.clearApplication();
-            System.out.println(clearResult);
+        GameUI.preLoginUI();
 
-            RegisterResult registerResult = serverFacade.register( new RegisterRequest( "username", "password", "email@gmail.com" ) );
-            System.out.println(registerResult);
 
-            LoginResult loginResult = serverFacade.login( new LoginRequest( "username", "password" ) );
-            System.out.println(loginResult);
-
-            CreateResult createResult = serverFacade.createGame( new CreateRequest( registerResult.authToken(), "game" ) );
-            System.out.println(createResult);
-
-            JoinResult joinResult = serverFacade.joinGame( new JoinRequest( loginResult.authToken(), "BLACK", createResult.gameID() ) );
-            System.out.println(joinResult);
-
-            ListResult listResult = serverFacade.listGames( new ListRequest( loginResult.authToken() ) );
-            System.out.println(listResult);
-
-            LogoutResult logoutResult = serverFacade.logout( new LogoutRequest( loginResult.authToken() ) );
-            System.out.println(logoutResult);
-
-            GameUI.drawGameBoard(listResult.games().getFirst() );
-            GameUI.drawGameBoard(new ChessBoard() );
-
-        } catch ( Error500Internal | URISyntaxException | IOException e ) {
-            throw new RuntimeException( e );
-        }
+//        ServerFacade serverFacade = new ServerFacade();
+//        try {
+//            ClearResult clearResult = serverFacade.clearApplication();
+//            System.out.println(clearResult);
+//
+//            RegisterResult registerResult = serverFacade.register( new RegisterRequest( "username", "password", "email@gmail.com" ) );
+//            System.out.println(registerResult);
+//
+//            LoginResult loginResult = serverFacade.login( new LoginRequest( "username", "password" ) );
+//            System.out.println(loginResult);
+//
+//            CreateResult createResult = serverFacade.createGame( new CreateRequest( registerResult.authToken(), "game" ) );
+//            System.out.println(createResult);
+//
+//            JoinResult joinResult = serverFacade.joinGame( new JoinRequest( loginResult.authToken(), "BLACK", createResult.gameID() ) );
+//            System.out.println(joinResult);
+//
+//            ListResult listResult = serverFacade.listGames( new ListRequest( loginResult.authToken() ) );
+//            System.out.println(listResult);
+//
+//            LogoutResult logoutResult = serverFacade.logout( new LogoutRequest( loginResult.authToken() ) );
+//            System.out.println(logoutResult);
+//
+//            GameUI.drawGameBoard(listResult.games().getFirst() );
+//            GameUI.drawGameBoard(new ChessBoard() );
+//
+//        } catch ( Error500Internal | URISyntaxException | IOException e ) {
+//            throw new RuntimeException( e );
+//        }
 //        serverFacade.initServer();
 
     }
--- client/src/main/java/ui/GameUI.java
+++ client/src/main/java/ui/GameUI.java
@@ -6,9 +6,117 @@ import chess.ChessGame;
 import datatypes.ExtendedChessBoard;
 import model.original.GameData;
 
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+import static ui.ChessUIConstants.*;
+
 
 public class GameUI {
     private static ChessBoard chessBoard = new ChessBoard();
+    private static PrintStream out = new PrintStream( System.out, true, StandardCharsets.UTF_8 );
+
+    private static void print(String string) {
+        out.print( string );
+    }
+
+    private static void printf(String string) {
+        out.printf( string );
+    }
+
+    private static void println(String string) {
+        out.println( string );
+    }
+
+    // Helper method to validate integer input
+    private static int getValidIntegerInput(Scanner scanner, int min, int max) {
+        int number;
+        // If the user types in a valid number, continue. Otherwise, keep scanning.
+        while (true) {
+            if (scanner.hasNextInt()) {
+                number = scanner.nextInt();
+                if (number >= min && number <= max) {
+                    break; // Exit the loop if input is valid
+                } else {
+                    println("Invalid input. Let's just enter an integer between " + min + " and " + max + ", shall we?.");
+                }
+            } else {
+                scanner.next(); // Discard the invalid input token
+                println( "Invalid input. Please just make my life easy and enter an integer." );
+            }
+        }
+        return number;
+    }
+
+    /*
+    Help	Displays text informing the user what actions they can take.
+    Quit	Exits the program.
+    Login	Prompts the user to input login information.
+            Calls the server login API to login the user.
+            When successfully logged in, the client should transition to the Postlogin UI.
+    Register	Prompts the user to input registration information.
+                Calls the server register API to register and login the user.
+                If successfully registered, the client should be logged in and transition
+                to the Postlogin UI.
+     */
+    public static void preLoginUI() {
+        Scanner scanner;
+        // Reset the terminal screen
+        print( ERASE_SCREEN );
+        TerminalUI.resetTerminalColors( out );
+        // Print out the menu
+        println( "Welcome to CS 240 Chess! Type 'Help' to get started.");
+        // If the user types help, continue. Otherwise, keep scanning.
+        while (true) {
+            scanner = new Scanner( System.in );
+            String inputString = scanner.nextLine();
+            if (inputString.equalsIgnoreCase( "Help" ))  {
+                println( "Good. You can follow instructions!" );
+                break;
+            }
+            println( "Invalid input. Please spare me the misery and just type 'Help'..." );
+        }
+        println( "1. Register");
+        println( "2. Login");
+        println( "3. Quit (and leave me in peace...highly recommended)");
+        println( "4. Help (and I'll be forced to print out this menu again)");
+        print("Please type your number: ");
+
+        // If the user types in a valid number, continue. Otherwise, keep scanning.
+        scanner = new Scanner( System.in );
+        int number = getValidIntegerInput( scanner, 1, 4);
+
+
+    }
+
+    // Help	Displays text informing the user what actions they can take.
+    // Logout	Logs out the user. Calls the server logout API to logout the user.
+    //          After logging out with the server, the client should transition to the Prelogin UI.
+    // Create Game	Allows the user to input a name for the new game.
+    //              Calls the server create API to create the game.
+    //              This does not join the player to the created game;
+    //              it only creates the new game in the server.
+    // List Games	Lists all the games that currently exist on the server.
+    //              Calls the server list API to get all the game data, and displays the
+    //              games in a numbered list, including the game name and players (not observers)
+    //              in the game. The numbering for the list should be independent of the game IDs.
+    // Play Game	Allows the user to specify which game they want to join and what color they
+    //              want to play. They should be able to enter the number of the desired game.
+    //              Your client will need to keep track of which number corresponds to which game
+    //              from the last time it listed the games. Calls the server join API to join the
+    //              user to the game.
+    // Observe Game	Allows the user to specify which game they want to observe.
+    //              They should be able to enter the number of the desired game.
+    //              Your client will need to keep track of which number corresponds to which game
+    //              from the last time it listed the games. Functionality will be added in Phase 6.
+    public static void postLoginUI() {
+
+    }
+
+    public static void gamePlayUI() {
+
+    }
 
     public static void drawGameBoard(ChessBoard board) {
         chessBoard = board;
@@ -23,4 +131,6 @@ public class GameUI {
         ChessBoardUI.drawBoard( chessBoard, ChessGame.TeamColor.WHITE );
         ChessBoardUI.drawBoard( chessBoard, ChessGame.TeamColor.BLACK );
     }
+
+
 }
