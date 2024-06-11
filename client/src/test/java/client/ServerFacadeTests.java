4cf6e2e1ce5138ddab095e0c4b67015cfff6251a
Joshua <jaredsbror@gmail.com>
2024-06-11 16:13:01 -0600

    (phase5) Fixed port to zero

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -42,7 +42,7 @@ public class ServerFacadeTests {
     @BeforeAll
     public static void init() {
         server = new Server();
-        var port = server.run(8080);
+        var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
         serverFacade = new ServerFacade();
     }
